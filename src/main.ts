70262268d85e79adfd165c881634e022af85897d
Jonatan Flyckt <fljo1589@student.ju.se>
2018-11-16 14:01:24 +0100

    Adds test for express.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("hello world");
+import * as express from "express";
+
+const app = express();
+
+app.get("/memes", (request, response) => {
+	response.status(200).json({ namn: "Flyckt", Age: 12 });
+});
+
+app.listen(80);
