d6a200b4cbaef9d9803d9f33b63cd720bb5a2202
Simon <zimonitrome@gmail.com>
2018-11-25 11:48:41 +0100

    Added routes for single meme and post.

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,17 @@ import memesRouter from "./routes/memes";
 import { getConnection } from "typeorm";
 import connectToDB from "./repository/dbConnection";
 import insertMockData from "./repository/insertMockData";
+import bodyParser from "body-parser";
 
 const port = process.env.PORT || 80;
 const app = express();
 
 (async () => {
 	await connectToDB();
-	await getConnection().synchronize(true);
+	await getConnection().synchronize(true); // Extra step which empties tables, rows, indices etc.
 	await insertMockData();
 
+	app.use(bodyParser.json());
 	app.use("/memes", memesRouter);
 	app.listen(port, () => console.log(`started webserver on port: ${port}`));
 })();
--- src/routes/memes.ts
+++ src/routes/memes.ts
@@ -1,25 +1,67 @@
 import express from "express";
-import { getRepository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 import { Meme } from "../repository/entities";
 
 const router = express.Router();
 
+router.post("/", async (request, response) => {
+	// TODO: validate parameters
+
+	try {
+		// TODO: Generate meme image here
+		const imageSource = "https://i.redd.it/po71lilrehky.jpg"; // temp
+		const meme = new Meme(
+			request.body.templateId,
+			request.body.username,
+			imageSource,
+			request.body.categoryId,
+			request.body.name
+		);
+
+		const memeRepo = getRepository(Meme);
+		const memes = await memeRepo.save(meme);
+		response.status(204).end();
+	} catch (error) {
+		console.error(error); // debugging
+		if (error.code === "23502") {
+			response.status(400).json({ errorMessage: "Missing parameter(s)." });
+		} else if (error.code === "22P02") {
+			response.status(400).json({ errorMessage: "Invalid parameter(s)." });
+		} else {
+			response.status(500).json("Internal server error.");
+		}
+	}
+});
+
 router.get("/", async (request, response) => {
-	const memeRepo = getRepository(Meme);
-	const memes = await memeRepo.find();
-	// const memes = await memeRepo.createQueryBuilder("meme").getMany();
-	response.status(200).json(memes);
+	try {
+		const memeRepo = getRepository(Meme);
+		const memes = await memeRepo.find();
+		response.status(200).json(memes);
+	} catch (error) {
+		console.error(error); // debugging
+		response.status(500).json("Internal server error.");
+	}
 });
 
 router.get("/:memeId", async (request, response) => {
-	const memeRepo = getRepository(Meme);
-	const memes = await memeRepo.find({
-		where: { id: request.params.memeId }
-	});
-	if (memes.length > 0) {
-		response.status(200).json(memes[0]);
-	} else {
-		response.status(404);
+	try {
+		const memeRepo = getRepository(Meme);
+		const meme = await memeRepo.findOne({
+			where: { id: request.params.memeId }
+		});
+		if (meme) {
+			response.status(200).json(meme);
+		} else {
+			response.status(404).end();
+		}
+	} catch (error) {
+		console.error(error); // debugging
+		if (error.code === "22P02") {
+			response.status(400).json({ errorMessage: "Invalid parameter(s)." });
+		} else {
+			response.status(500).json("Internal server error.");
+		}
 	}
 });
 
