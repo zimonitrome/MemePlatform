4a945a0da70e39984b156302fe6e095837ae0c59
jonatan-flyckt <jonatan.flyckt@hotmail.com>
2018-12-01 22:42:51 +0100

    Adds validation for vote class.

--- src/repository/entities/vote.ts
+++ src/repository/entities/vote.ts
@@ -1,11 +1,34 @@
-import { Entity, Column, PrimaryColumn } from "typeorm";
+import { Entity, Column, PrimaryColumn, getRepository } from "typeorm";
+import { Meme, User } from ".";
 
 @Entity()
 export class Vote {
 	constructor(vote: number, memeId: number, username: string) {
-		this.vote = vote;
-		this.memeId = memeId;
-		this.username = username;
+		if (vote === -1 || vote === 1) {
+			this.vote = vote;
+		} else {
+			throw new Error("Vote must be -1 or 1.");
+		}
+		if (memeId) {
+			const memeRepo = getRepository(Meme);
+			if (memeRepo.find({ where: { id: memeId } })) {
+				this.memeId = memeId;
+			} else {
+				throw new Error("Meme does not exist.");
+			}
+		} else {
+			throw new Error("Missing parameter memeId.");
+		}
+		if (username) {
+			const userRepo = getRepository(User);
+			if (userRepo.find({ where: { username } })) {
+				this.username = username;
+			} else {
+				throw new Error("User does not exist.");
+			}
+		} else {
+			throw new Error("Missing parameter username.");
+		}
 	}
 
 	@Column()
